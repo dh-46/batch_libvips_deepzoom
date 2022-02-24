019ad2a96e236bc8beed68456e4fec9c3634a712
Daniel (EMCT) <daniel.h@emct.com.tw>
2022-02-24 14:30:32 +0800

    Feat: 原始圖片放大兩倍後再執行圖磚產製

--- run_deep_zoom.bat
+++ run_deep_zoom.bat
@@ -1,9 +1,21 @@
 md %cd%\output
+md %cd%\enlarge
 set outputDir=%cd%\output
+set enlargeDir=%cd%\enlarge
+
+rem ENLARGE PICTURES IN INPUTS FOLDER
 for /r %%f in (input\*) do (
     echo %%f
     echo %%~nf
     cd .\libvips\bin\
-    start /wait vips dzsave %%f %outputDir%\%%~nf --layout google --background 0 --suffix .webp
+    start /wait vips resize %%f %enlargeDir%\%%~nxf 10
+)
+
+rem MAKE DEEP ZOOM TILES FOR PICTURES IN ENLARGE FOLDER
+for %%d in ( %enlargeDir%\*) do (
+    echo %%d
+    echo %%~nd
+    cd .\libvips\bin\
+    start /wait vips dzsave %%d %outputDir%\%%~nd --layout google --background 0 --suffix .webp
 )
 pause
